997ba835f78572304da23b7c5714f3b1a813d13e
imbrahim01 <130819291+imbrahim01@users.noreply.github.com>
2024-10-31 00:57:19 +0700

    Update streamzema.py

--- streamzema.py
+++ streamzema.py
@@ -13,7 +13,7 @@ st.title("Pengolahan Citra Medika")
 
 # Sidebar menu
 with st.sidebar:
-    selected = option_menu(["Home", "Encyclopedia", "Feature Extraction", "Chatbot"], default_index=0)
+    selected = option_menu("Pengolahan Citra Medika",["Home", "Encyclopedia", "Feature Extraction", "Chatbot"], default_index=0)
 
 # Home Page
 if selected == "Home":
